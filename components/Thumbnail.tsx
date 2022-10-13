3b2afc2250088931a65c5b1754e5523df732eb25
Oluwasegun <adeolusegun99@gmail.com>
2022-10-13 02:44:06 +0100

    changed text color to black

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -16,7 +16,7 @@ const Thumbnail = ({ movie }: Props) => {
         className="object-cover rounded-sm md:rounded"
         layout="fill"
       />
-      <h4 className="absolute text-sm text-center text-white top-24">
+      <h4 className="absolute text-sm text-center text-black top-24">
         {movie?.title?.substring(0, 16) || movie?.name?.substring(0, 16)}
       </h4>
     </div>
